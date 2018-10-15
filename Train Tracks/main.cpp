c1d25bd799c96673ca33d8a7dce2e1c680f8ea68
Alex Lowen <alexlowen99@gmail.com>
2018-10-15 17:23:03 -0400

    Initial Commit

---
+++ Train Tracks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Train Tracks
+//
+//  Created by Alex Lowen on 10/15/18.
+//  Copyright (c) 2018 Alex Lowen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
